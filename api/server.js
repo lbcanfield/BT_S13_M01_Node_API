0a5e0d5ca165784fdf61d2c10b21103801e4b361
lbcanfield <lewie.b.canfield@gmail.com>
2023-06-09 15:05:09 -0500

    /root/index.js - server.listen( ) method created /root/package.json - server script created /root/server.js - express variable defined requiring express - USERS variable declared requiring the users model - server variable declared invoking the express function - [GET] verb defined to get users from the users model - catch all created.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,29 @@
-// BUILD YOUR SERVER HERE
+const express = require('express');
+const USERS = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+// [GET] - gets all users from the model
+server.get('/api/users/', async (request, response) => {
+     try {
+          const users = await USERS.find();
+          response.status(200).json(users)
+     }
+     catch (error) {
+          response.status(500).json({
+               message: "The users information could not be retrieved",
+               error: error.message,
+          })
+     }
+})
+
+
+// Catch all when nothing specific is specified
+server.use('*', (request, response) => {
+     response.status(404).json({
+          message: 'not found'
+     })
+})
+
+
+module.exports = server; 
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./api/server');
 
+//Declaring the port number that the server will use.
 const port = 9000;
 
-// START YOUR SERVER HERE
+
+//Function to allow the server to listen on the above declared port number
+server.listen(port, () => {
+     console.log(`listening on port ${port}`);
+});
